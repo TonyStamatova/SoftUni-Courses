b13c11883ceced70f757bb12879b2654ac1430ef
TonyStamatova <a.stamatova@abv.bg>
2019-03-31 23:39:19 +0300

    Added files

---
+++ Technology-Fundamentals/Homework-Solutions/08.TextProcessingAndRegularExpressions/textRegexEx/04.replaceRepeatingChars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.replaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(input[0]);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i-1])
+                {
+                    builder.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(builder);
+        }
+    }
+}
---
+++ Technology-Fundamentals/Homework-Solutions/08.TextProcessingAndRegularExpressions/textRegexEx/05.stringExplosion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.stringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    int strength = int.Parse(input[i + 1].ToString());
+
+                    for (int j = i; j < i + strength; j++)
+                    {
+                        if (input[j] == '>')
+                        {
+                            strength += int.Parse(input[j + 1].ToString());
+                        }
+                    }
+
+                    input.Remove(input[i + 1], Math.Min(strength, input.Length - i - 1));
+                }                
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
