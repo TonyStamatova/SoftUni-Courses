3ad9b80342b2ae62e05f07a35a89b4d1eed096b1
TonyStamatova <a.stamatova@abv.bg>
2019-04-02 00:05:24 +0300

    Added files

---
+++ Technology-Fundamentals/Homework-Solutions/06.Lists/techListsEx/03.houseParty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.houseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                if (command.Length == 3)
+                {
+                    if (guests.Contains(command[0]))
+                    {
+                        Console.WriteLine($"{command[0]} is already in the list!");
+                        continue;
+                    }
+
+                    guests.Add(command[0]);
+                    continue;
+                }
+
+                if (guests.Contains(command[0]))
+                {
+                    guests.Remove(command[0]);
+                    continue;
+                }
+
+                Console.WriteLine($"{command[0]} is not in the list!");
+            }
+
+            foreach (var item in guests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Technology-Fundamentals/Homework-Solutions/06.Lists/techListsEx/04.listOperations/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.listOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = string.Empty;
+            int index = default(int);
+            
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] commandArr = command.Split();
+
+                switch (commandArr[0])
+                {
+                    case "Add":
+                        list.Add(int.Parse(commandArr[1]));
+                        break;
+                    case "Insert":
+                        index = int.Parse(commandArr[2]);
+                        if (CheckIfIndexIsValid(index, list))
+                        {
+                            list.Insert(index, int.Parse(commandArr[1]));
+                        }
+                        break;
+                    case "Remove":
+                        index = int.Parse(commandArr[1]);
+                        if (CheckIfIndexIsValid(index, list))
+                        {
+                            list.RemoveAt(index);
+                        }
+                        break;
+                    case "Shift":
+                        switch (commandArr[1])
+                        {
+                            case "left":
+                                list = Switch(list, int.Parse(commandArr[2]), 0, list.Count - 1);
+                                break;
+                            case "right":
+                                list = Switch(list, int.Parse(commandArr[2]), list.Count - 1, 0);
+                                break;
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+
+        private static List<int> Switch(List<int> list, int count, int indexToTakeFrom, int indexToInsertOn)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                int current = list[indexToTakeFrom];
+                list.RemoveAt(indexToTakeFrom);
+                list.Insert(indexToInsertOn, current);
+            }
+
+            return list;
+        }
+
+        private static bool CheckIfIndexIsValid(int index, List<int> list)
+        {
+            if (index > list.Count || index < 0)
+            {
+                Console.WriteLine("Invalid index");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
