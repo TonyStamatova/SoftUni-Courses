d12a474f37e2bcd3c8c3a568e1d2bb0acaaee02f
TonyStamatova <a.stamatova@abv.bg>
2019-04-02 00:00:59 +0300

    Added files

---
+++ Technology-Fundamentals/Homework-Solutions/06.Lists/listsEx/longestSubsequence/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        int maxNumber = 0;
+        int maxCount = 0;
+
+        int currentCount = 1;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            for (int j = i + 1; j < numbers.Count; j++)
+            {
+                if (numbers[j] == numbers[i])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (currentCount > maxCount)
+            {
+                maxNumber = numbers[i];
+                maxCount = currentCount;
+            }
+
+            currentCount = 1;
+        }
+
+        for (int i = 0; i < maxCount; i++)
+        {
+            Console.Write(maxNumber + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
