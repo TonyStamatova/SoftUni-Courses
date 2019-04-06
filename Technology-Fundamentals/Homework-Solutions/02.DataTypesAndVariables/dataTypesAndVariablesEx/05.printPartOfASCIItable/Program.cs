a1f784a0dd56ec94c2fbb777a80e89c8487adcc9
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-04-06 23:54:22 +0300

    Add files via upload
    Added files

---
+++ Technology-Fundamentals/Homework-Solutions/02.DataTypesAndVariables/dataTypesAndVariablesEx/05.printPartOfASCIItable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.printPartOfASCIItable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstCharNum = int.Parse(Console.ReadLine());
+            int lastCharNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstCharNum; i <= lastCharNum; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
