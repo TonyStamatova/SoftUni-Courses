c361911ad37d4d689d5c4ca8404aa6ec20be1418
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-04-06 23:43:54 +0300

    Add files via upload
    Added files

---
+++ Technology-Fundamentals/Homework-Solutions/01.BasicSyntax/basicSyntaxEx/06.strongNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06.strongNumber
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int currentNum = num;
+            int digit = default(int);
+            int sum = default(int);
+            int factorial = 1;
+
+            while (currentNum != 0)
+            {
+                digit = currentNum % 10;
+
+                for (int i = 2; i <= digit; i++)
+                {
+                    factorial *= i;
+                }
+
+                sum += factorial;
+                currentNum /= 10;
+                factorial = 1;
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
