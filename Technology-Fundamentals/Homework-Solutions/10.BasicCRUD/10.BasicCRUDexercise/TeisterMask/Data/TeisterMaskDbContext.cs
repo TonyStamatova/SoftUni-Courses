325e071b0683c85ad8b4fe516bc18463d417526a
TonyStamatova <a.stamatova@abv.bg>
2019-04-11 21:52:42 +0300

    Added files

---
+++ Technology-Fundamentals/Homework-Solutions/10.BasicCRUD/10.BasicCRUDexercise/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TeisterMask.Models;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContext: DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-VN8E4A8\SQLEXPRESS;Database=MeisterTaskDbContext;Integrated Security=True;");
+        }
+    }
+}
