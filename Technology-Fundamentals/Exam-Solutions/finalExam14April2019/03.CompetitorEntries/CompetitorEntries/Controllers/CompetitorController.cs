e562b633e09264c9492503dc9aeac00baf7a8c45
TonyStamatova <a.stamatova@abv.bg>
2019-04-16 22:09:53 +0300

    Added files

---
+++ Technology-Fundamentals/Exam-Solutions/finalExam14April2019/03.CompetitorEntries/CompetitorEntries/Controllers/CompetitorController.cs
@@ -0,0 +1,128 @@
+﻿using CompetitorEntries.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CompetitorEntries.Controllers
+{
+    public class CompetitorController : Controller
+    {
+        private readonly CompetitorEntriesDbContext context;
+
+        public CompetitorController(CompetitorEntriesDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            using (CompetitorEntriesDbContext db = new CompetitorEntriesDbContext())
+            {
+                var allCompetitors = db.Competitors.ToList();
+                return this.View(allCompetitors);
+            }            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Competitor competitor)
+        {
+            if (competitor == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var newCompetitor = new Competitor
+            {
+                Name = competitor.Name,
+                Age = competitor.Age,
+                Category = competitor.Category,
+                Team = competitor.Team
+            };
+
+            using (CompetitorEntriesDbContext db = new CompetitorEntriesDbContext())
+            {                
+                db.Competitors.Add(newCompetitor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }               
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (CompetitorEntriesDbContext db = new CompetitorEntriesDbContext())
+            {
+                var compToEdit = db.Competitors.FirstOrDefault(x => x.Id == id);
+
+                if (compToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(compToEdit);
+            }
+               
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Competitor competitor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            using (CompetitorEntriesDbContext db = new CompetitorEntriesDbContext())
+            {
+                var compToEdit = db.Competitors.FirstOrDefault(x => x.Id == competitor.Id);
+                compToEdit.Name = competitor.Name;
+                compToEdit.Team = competitor.Team;
+                compToEdit.Age = competitor.Age;
+                compToEdit.Category = competitor.Category;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (CompetitorEntriesDbContext db = new CompetitorEntriesDbContext())
+            {
+                var compToDelete = db.Competitors.FirstOrDefault(x => x.Id == id);
+
+                if (compToDelete == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(compToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Competitor competitor)
+        {
+            using (CompetitorEntriesDbContext db = new CompetitorEntriesDbContext())
+            {
+                var compToDelete = db.Competitors.FirstOrDefault(x => x.Id == competitor.Id);
+
+                if (compToDelete == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                db.Competitors.Remove(compToDelete);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Technology-Fundamentals/Exam-Solutions/finalExam14April2019/03.CompetitorEntries/CompetitorEntries/Models/Competitor.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompetitorEntries.Models
+{
+    public class Competitor
+    {
+        //       •	name – non-empty text
+        //•	age – non-null integer number
+        //•	team – non-empty text
+        //•	category – non-empty text
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Team { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+    }
+}
