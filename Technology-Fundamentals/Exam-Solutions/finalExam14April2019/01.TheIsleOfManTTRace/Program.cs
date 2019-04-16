98b427c29bad3e4d7dca9d7cd59e7dbb57387831
TonyStamatova <a.stamatova@abv.bg>
2019-04-16 22:07:57 +0300

    Added files

---
+++ Technology-Fundamentals/Exam-Solutions/finalExam14April2019/01.TheIsleOfManTTRace/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                Match nameMatch = Regex.Match(input, @"(?<=([#\$%\*&]{1}))(?<name>([A-Za-z]+))\1=(?<length>([0-9]+))!!(?<geohash>(.+))$");
+
+                if (nameMatch.ToString() == "")
+                {
+                    Console.WriteLine("Nothing found!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                int length = int.Parse(nameMatch.Groups["length"].ToString());
+                string geohash = nameMatch.Groups["geohash"].ToString();
+
+                if (geohash.Length != length)
+                {
+                    Console.WriteLine("Nothing found!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < geohash.Length; i++)
+                {
+                    char charToAppend = (char)(geohash[i] + length);
+                    builder.Append(charToAppend);
+                }
+
+                string name = nameMatch.Groups["name"].ToString();
+                Console.WriteLine($"Coordinates found! {name} -> {builder}");
+                break;
+            }
+        }
+    }
+}
---
+++ Technology-Fundamentals/Exam-Solutions/finalExam14April2019/02.PracticeSessions/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, List<string>> roads = new Dictionary<string, List<string>>();
+
+            while (input != "END")
+            {
+                string[] data = input.Split("->");
+                string command = data[0];
+                string currentRoad = data[1];
+                string racer = data[2];
+
+                switch (command)
+                {
+                    case "Add":
+                        if (!roads.ContainsKey(currentRoad))
+                        {
+                            roads.Add(currentRoad, new List<string>());
+                        }
+                        roads[currentRoad].Add(racer);
+                        break;
+                    case "Move":
+                        if (roads.ContainsKey(currentRoad) && roads[currentRoad].Contains(racer))
+                        {
+                            roads[currentRoad].Remove(racer);
+                            string newRoad = data[3];
+                            roads[newRoad].Add(racer);
+                        }
+                        break;
+                    case "Close":
+                        if (roads.ContainsKey(currentRoad))
+                        {
+                            roads.Remove(currentRoad);
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Practice sessions:");
+            foreach (var road in roads.OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key))
+            {
+                Console.WriteLine(road.Key);
+
+                foreach (var racer in road.Value)
+                {
+                    Console.WriteLine($"++{racer}");
+                }
+            }
+        }
+    }
+}
