02aab2adfd85bff48aabec629d69b18a60d4d62e
TonyStamatova <a.stamatova@abv.bg>
2019-04-16 22:06:59 +0300

    Added files

---
+++ Technology-Fundamentals/Exam-Solutions/finalExam16december2018/01.Concert/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, Band> bandsByName = new Dictionary<string, Band>();
+
+            while (input != "start of concert")
+            {
+                string[] data = input.Split("; ");
+                string band = data[1];
+
+                if (!bandsByName.ContainsKey(band))
+                {
+                    bandsByName.Add(band, new Band(band));
+                }
+                
+                string command = data[0];
+
+                switch (command)
+                {
+                    case "Add":
+                        string[] members = data[2].Split(", ");
+                        foreach (var member in members)
+                        {
+                            if (!bandsByName[band].Members.Any(x => x == member))
+                            {
+                                bandsByName[band].Members.Add(member);
+                            }                            
+                        }
+                        break;
+
+                    case "Play":
+                        int time = int.Parse(data[2]);
+                        bandsByName[band].Time += time;
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int totTime = default(int);
+
+            foreach (var band in bandsByName.Values)
+            {
+                totTime += band.Time;
+            }
+
+            Console.WriteLine($"Total time: {totTime}");
+
+            foreach (var band in bandsByName.Values.OrderByDescending(x => x.Time)
+                .ThenBy(x => x.Name))
+            {
+                Console.WriteLine($"{band.Name} -> {band.Time}");
+            }
+
+            string bandName = Console.ReadLine();
+
+            Console.Write(bandName + Environment.NewLine + "=> ");
+            Console.WriteLine(string.Join(Environment.NewLine + "=> ", bandsByName[bandName].Members));
+        }
+    }
+
+    class Band
+    {
+        public Band(string name)
+        {
+            this.Name = name;
+            this.Time = 0;
+            this.Members = new List<string>();
+        }
+
+        public string Name { get; set; }
+
+        public int Time { get; set; }
+
+        public List<string> Members { get; set; }
+    }
+}
---
+++ Technology-Fundamentals/Exam-Solutions/finalExam16december2018/02.SongEncription/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _02.songEncription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                Match artist = Regex.Match(input, @"(?<=^)[A-Z]{1}[a-z' ]+(?=:)");
+                Match song = Regex.Match(input, @"(?<=:)[A-Z ]+(?=$)");
+
+                if (artist.ToString() == "" || song.ToString() == "")
+                {
+                    Console.WriteLine("Invalid input!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                int key = artist.Length;
+
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char newChar = input[i];
+
+                    if (Char.IsLetter(newChar))
+                    {
+                        if (Char.IsLower(newChar))
+                        {
+                            for (int j = 0; j < key; j++)
+                            {
+                                newChar++;
+
+                                if (newChar == 'z' && j < key - 1)
+                                {
+                                    j++;
+                                    newChar = 'a';
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int j = 0; j < key; j++)
+                            {
+                                newChar++;
+
+                                if (newChar == 'Z' && j < key - 1)
+                                {
+                                    j++;
+                                    newChar = 'A';
+                                }
+                            }
+                        }
+                    }
+                    else if (newChar == ':')
+                    {
+                        newChar = '@';
+                    }
+
+                    builder.Append(newChar);
+                }
+
+                Console.WriteLine($"Successful encryption: {builder}");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
