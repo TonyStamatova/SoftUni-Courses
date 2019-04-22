21d6e461af720303fbbb0e6d6e3ab9bbf0838d8e
TonyStamatova <a.stamatova@abv.bg>
2019-04-22 22:39:06 +0300

    Uploaded AVL Tree implementation

---
+++ Data-Structures/Homework-Solutions/07.AVLTree/AVL.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+public class AVL<T> where T : IComparable<T>
+{
+    private Node<T> root;
+
+    public Node<T> Root
+    {
+        get
+        {
+            return this.root;
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        var node = this.Search(this.root, item);
+        return node != null;
+    }
+
+    public void Insert(T item)
+    {
+        this.root = this.Insert(this.root, item);
+    }
+
+    public void Delete(T item)
+    {
+        this.root = this.Delete(this.root, item);
+    }
+
+    private Node<T> Delete(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        Node<T> result;
+        if (cmp == 0)
+        {
+            if (node.Left != null)
+            {
+                if (node.Left.Right != null)
+                {
+                    node.Left.Right.Right = node.Right;
+                    result = RotateLeft(node.Left);
+                    result = Balance(result);
+                    UpdateHeight(result);
+                    return result;
+                }
+
+                node.Left.Right = node.Right;
+                result = node.Left;
+                result = Balance(result);
+                UpdateHeight(result);
+                return result;
+            }
+
+            if (node.Right != null)
+            {
+                if (node.Right.Left != null)
+                {
+                    node.Right.Left.Left = node.Left;
+                    result = RotateRight(node.Right);
+                    result = Balance(result);
+                    UpdateHeight(result);
+                    return result;
+                }
+
+                node.Right.Left = node.Left;
+                result = node.Right;
+                result = Balance(result);
+                UpdateHeight(result);
+                return result;
+            }
+
+            node = null;
+        }
+        else if (cmp < 0)
+        {
+            node.Left = this.Delete(node.Left, item);
+        }
+        else
+        {
+            node.Right = this.Delete(node.Right, item);
+        }
+
+        node = Balance(node);
+        UpdateHeight(node);
+        return node;
+    }
+
+    public void DeleteMin()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private Node<T> Insert(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return new Node<T>(item);
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        if (cmp < 0)
+        {
+            node.Left = this.Insert(node.Left, item);
+        }
+        else if (cmp > 0)
+        {
+            node.Right = this.Insert(node.Right, item);
+        }
+
+        node = Balance(node);
+        UpdateHeight(node);
+        return node;
+    }
+
+    private Node<T> Balance(Node<T> node)
+    {
+        var balance = Height(node.Left) - Height(node.Right);
+        if (balance > 1)
+        {
+            var childBalance = Height(node.Left.Left) - Height(node.Left.Right);
+            if (childBalance < 0)
+            {
+                node.Left = RotateLeft(node.Left);
+            }
+
+            node = RotateRight(node);
+        }
+        else if (balance < -1)
+        {
+            var childBalance = Height(node.Right.Left) - Height(node.Right.Right);
+            if (childBalance > 0)
+            {
+                node.Right = RotateRight(node.Right);
+            }
+
+            node = RotateLeft(node);
+        }
+
+        return node;
+    }
+
+    private void UpdateHeight(Node<T> node)
+    {
+        node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+    }
+
+    private Node<T> Search(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        if (cmp < 0)
+        {
+            return Search(node.Left, item);
+        }
+        else if (cmp > 0)
+        {
+            return Search(node.Right, item);
+        }
+
+        return node;
+    }
+
+    private void EachInOrder(Node<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Value);
+        this.EachInOrder(node.Right, action);
+    }
+
+    private int Height(Node<T> node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        return node.Height;
+    }
+
+    private Node<T> RotateRight(Node<T> node)
+    {
+        var left = node.Left;
+        node.Left = left.Right;
+        left.Right = node;
+
+        UpdateHeight(node);
+
+        return left;
+    }
+
+    private Node<T> RotateLeft(Node<T> node)
+    {
+        var right = node.Right;
+        node.Right = right.Left;
+        right.Left = node;
+
+        UpdateHeight(node);
+
+        return right;
+    }
+}
---
+++ Data-Structures/Homework-Solutions/07.AVLTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AVLTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVL<int> tree = new AVL<int>();
+            tree.Insert(1);
+            tree.Insert(3);
+            tree.Insert(2);
+
+            Console.WriteLine();
+        }
+    }
+}
