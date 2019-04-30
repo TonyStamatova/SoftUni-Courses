93cdd4ef2dcb071fe17d87f8c8d74d4ce61e2bc2
TonyStamatova <a.stamatova@abv.bg>
2019-04-30 22:35:35 +0300

    Added unfinished web application; changed files location

---
+++ Technology-Fundamentals/Homework-Solutions/11.WebProject/SoftUniTwitter/Controllers/TweetsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftUniTwitter.Data;
+using SoftUniTwitter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SoftUniTwitter.Controllers
+{
+    public class TweetsController : Controller
+    {
+        ApplicationDbContext db;
+
+        public TweetsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        // TODO: Logged user?
+
+        public IActionResult SaveToDatabase(string text)
+        {
+            var tweet = new Tweet
+            {
+                Text = text,
+                CreatedOn = DateTime.UtcNow,
+                UserId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value
+            };
+
+            db.Tweets.Add(tweet);
+            db.SaveChanges();
+
+            return Redirect("/");
+        }
+    }
+}
