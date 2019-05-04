f4b1c912307354e063f9696db6536e5800767697
TonyStamatova <a.stamatova@abv.bg>
2019-05-04 18:56:46 +0300

    Changed folder numeration

---
+++ Technology-Fundamentals/Homework-Solutions/05.Lists/listsExercise/02proba/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+            List<string> wordList = new List<string>(words.Split(' '));
+            wordList.Sort((a, b) => a.CompareTo(b));
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                Console.Write(wordList[i] + " ");
+            }
+        }
+    }
+}
---
+++ Technology-Fundamentals/Homework-Solutions/05.Lists/listsExercise/countOfOccurrences/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = Console.ReadLine()
+        .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => int.Parse(x)).ToList();
+
+        numbers.Sort();
+
+        List<int> occurredNums = new List<int>();
+
+        int occurrences = 1;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+
+            if (!occurredNums.Contains(numbers[i]))
+            {
+                for (int j = i+1; j < numbers.Count; j++)
+                {
+                    if (numbers[j] == numbers[i])
+                    {
+                        occurrences++;
+                    }
+                }
+                Console.WriteLine($"{numbers[i]} -> {occurrences} times");
+
+                occurredNums.Add(numbers[i]);
+            }
+
+            occurrences = 1;
+        }
+    }
+}
+
