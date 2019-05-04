162e3e8d78346a620e88c267b6fdd59ae265517a
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-04 18:48:49 +0300

    Added solutions for other homework problems

---
+++ Technology-Fundamentals/Homework-Solutions/02.DataTypesAndVariables/DataTypesAndVariablesMoreEx/01.DataTypeFinder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _01.DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intType;
+            float floatType;
+            char charType;
+            bool boolType;
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out intType))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out floatType))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out charType))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out boolType))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Technology-Fundamentals/Homework-Solutions/02.DataTypesAndVariables/DataTypesAndVariablesMoreEx/02.FromLeftToTheRight/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _02.FromLeftToTheRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+
+                int counter = 0;
+                string firstNumAsString = string.Empty;
+
+                while (input[counter] != ' ')
+                {
+                    firstNumAsString += input[counter];
+                    counter++;
+                }
+
+                counter++;
+
+                string secondNumAsString = string.Empty;
+
+                while (counter < input.Length)
+                {
+                    secondNumAsString += input[counter];
+                    counter++;
+                }
+
+                long firstNum = long.Parse(firstNumAsString);
+                long secondNum = long.Parse(secondNumAsString);
+                long maxNum = Math.Abs(Math.Max(firstNum, secondNum));                
+                int sum = default(int);
+
+                while (maxNum != 0)
+                {
+                    int digit = (int)(maxNum % 10);
+                    sum += digit;
+                    maxNum /= 10;
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
