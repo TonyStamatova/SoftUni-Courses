97d866cbf80926cb419f73a6a94442d6a2383484
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-04 23:11:17 +0300

    Added folder

---
+++ Technology-Fundamentals/Homework-Solutions/MethodsExercise/04.PasswordValidator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _04.passwordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool charCountIsValid = CheckCountOfChars(password);
+            bool contentIsValid = CheckContent(password);
+            bool minDigitCount = CheckMinDigitCount(password);
+
+            if (!charCountIsValid)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+
+            if (!contentIsValid)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+
+            if (!minDigitCount)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+
+            if (charCountIsValid && contentIsValid && minDigitCount)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        private static bool CheckMinDigitCount(string password)
+        {
+            int digitCount = default(int);
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsDigit(password[i]))
+                {
+                    digitCount++;
+                }
+            }
+
+            return digitCount < 2 ? false : true;
+        }
+
+        private static bool CheckContent(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (!Char.IsDigit(password[i]) && !Char.IsLetter(password[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CheckCountOfChars(string password)
+        {
+            if (password.Length < 6 || password.Length > 10)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Technology-Fundamentals/Homework-Solutions/MethodsExercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09.palindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                Console.WriteLine(IsPalindrome(input).ToString().ToLower());
+            }
+        }
+
+        private static bool IsPalindrome(string inputString)
+        {
+            for (int i = 0; i < inputString.Length / 2; i++)
+            {
+                if (!Char.IsDigit(inputString[i]))
+                {
+                    return false;
+                }
+
+                if (inputString[i] != inputString[inputString.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
