119e79af024584c918f2c0c2c827c3be32bd380d
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-16 22:01:14 +0300

    Added a project

---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesLab/05.Supermarket/Supermarket.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.Supermarket
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Supermarket
+    {
+        public static void Main()
+        {
+            Queue<string> queue = new Queue<string>();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while(queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
