9c900ae1126345ab899ffeee8fd1050b9e630f74
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-16 21:42:48 +0300

    Added new projects

---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesLab/02.StackSum/StackSum.cs
@@ -0,0 +1,51 @@
+﻿namespace _02.StackSum
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StackSum
+    {
+        public static void Main()
+        {
+            Stack<int> numbers = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+            string line = Console.ReadLine();
+
+            while (line.ToLower() != "end")
+            {
+                string[] commandLine = line.Split();
+                string command = commandLine[0].ToLower();
+
+                switch (command)
+                {
+                    case "add":
+                        for (int i = 1; i <= 2; i++)
+                        {
+                            numbers.Push(int.Parse(commandLine[i]));
+                        }
+                        break;
+
+                    case "remove":
+                        int elementsToRemove = int.Parse(commandLine[1]);
+
+                        if (elementsToRemove <= numbers.Count)
+                        {
+                            for (int i = 0; i < elementsToRemove; i++)
+                            {
+                                numbers.Pop();
+                            }
+                        }
+
+                        break;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Sum: {numbers.Sum()}");
+        }
+    }
+}
---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesLab/03.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,38 @@
+﻿namespace _03.SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> elements = new Stack<string>(input.Reverse());
+
+            while (elements.Count > 1)
+            {
+                int leftOperand = int.Parse(elements.Pop());
+                string operatorr = elements.Pop();
+                int rightOperand = int.Parse(elements.Pop());
+                int result = default(int);
+
+                switch (operatorr)
+                {
+                    case "+":
+                        result = leftOperand + rightOperand;
+                        break;
+                    case "-":
+                        result = leftOperand - rightOperand;
+                        break;
+                }
+
+                elements.Push(result.ToString());
+            }
+
+            Console.WriteLine(elements.Pop());
+        }
+    }
+}
