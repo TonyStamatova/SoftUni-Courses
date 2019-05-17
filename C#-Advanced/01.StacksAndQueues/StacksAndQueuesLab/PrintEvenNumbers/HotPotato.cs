c30771831900ed578c6844586625ae6cd3cd01bc
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-17 20:26:00 +0300

    Added projects

---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesLab/PrintEvenNumbers/HotPotato.cs
@@ -0,0 +1,31 @@
+﻿namespace PrintEvenNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PrintEvenNumbers
+    {
+        public static void Main()
+        {
+            Queue<int> numbers = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            int count = numbers.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = numbers.Dequeue();
+
+                if (number % 2 == 0)
+                {
+                    numbers.Enqueue(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
