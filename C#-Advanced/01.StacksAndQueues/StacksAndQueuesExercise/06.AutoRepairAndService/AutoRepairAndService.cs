0737599fa244e12971135a9c0529940030bd9266
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-18 23:14:57 +0300

    Added projects

---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesExercise/06.AutoRepairAndService/AutoRepairAndService.cs
@@ -0,0 +1,63 @@
+﻿namespace _06.AutoRepairAndService
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class AutoRepairAndService
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> waitingVehicles = new Queue<string>(Console.ReadLine()
+                .Split());
+
+            string command = Console.ReadLine();
+
+            Stack<string> served = new Stack<string>();
+
+            while (command != "End")
+            {
+                switch (command)
+                {
+                    case "Service":
+
+                        if (waitingVehicles.Count > 0)
+                        {
+                            string car = waitingVehicles.Dequeue();
+                            served.Push(car);
+                            Console.WriteLine($"Vehicle {car} got served.");
+                        }          
+                        
+                        break;
+
+                    case "History":
+                        Console.WriteLine($"{string.Join(", ", served)}");
+                        break;
+
+                    default:
+                        string[] request = command.Split("-");
+                        string carName = request[1];
+
+                        if (waitingVehicles.Contains(carName))
+                        {
+                            Console.WriteLine("Still waiting for service.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Served.");
+                        }
+
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (waitingVehicles.Count > 0)
+            {
+                Console.WriteLine($"Vehicles for service: {string.Join(", ", waitingVehicles)}");
+            }
+
+            Console.WriteLine($"Served vehicles: {string.Join(", ", served)}");
+        }
+    }
+}
