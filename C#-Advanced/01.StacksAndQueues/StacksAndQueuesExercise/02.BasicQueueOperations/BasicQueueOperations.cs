6dad82eb6b717de0b8779ff5e224e7f57a22cf48
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-18 23:12:23 +0300

    Added solution folder

---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesExercise/02.BasicQueueOperations/BasicQueueOperations.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.BasicQueueOperations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BasicQueueOperations
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            int dequeueCount = input[1];
+
+            for (int i = 0; i < dequeueCount; i++)
+            {
+                queue.Dequeue();
+            }
+
+            int elementToFind = input[2];
+
+            if (queue.Contains(elementToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Count > 0 ? queue.Min() : 0);
+            }
+        }
+    }
+}
