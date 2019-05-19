d89ce0897e032ac0c1b7cb19f5bf315dc3cbc146
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-19 15:57:05 +0300

    Added projects

---
+++ C#-Advanced/01.StacksAndQueues/StacksAndQueuesExercise/11.KeyRevolver/KeyRevolver.cs
@@ -0,0 +1,73 @@
+﻿namespace _11.KeyRevolver
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class KeyRevolver
+    {
+        public static void Main()
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            Stack<int> bullets = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+            Queue<int> locks = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+            int money = int.Parse(Console.ReadLine());
+            bool noLocksLeft = false;
+
+            while (locks.Count > 0)
+            {
+                if (bullets.Count == 0)
+                {
+                    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+                    return;
+                }
+
+                int bulletsInBarrel = Math.Min(bullets.Count, barrelSize);
+
+                while (bulletsInBarrel > 0)
+                {
+                    int bulletSize = bullets.Pop();
+                    bulletsInBarrel--;
+                    money -= bulletPrice;
+                    int lockSize = locks.Peek();
+
+                    if (bulletSize <= lockSize)
+                    {
+                        locks.Dequeue();
+                        Console.WriteLine("Bang!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ping!");
+                    }
+
+                    if (locks.Count == 0)
+                    {
+                        noLocksLeft = true;
+                        break;
+                    }
+                }
+
+                if (bullets.Count > 0 && bulletsInBarrel == 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+
+                if (noLocksLeft)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine($"{bullets.Count} bullets left. Earned ${money}");
+        }
+    }
+}
