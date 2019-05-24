567f1723cc143f4833a7e858be9e6eae5e0697da
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-24 20:42:01 +0300

    Added problem solutions

---
+++ C#-Advanced/03.SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedLab/01.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.CountSameValuesInArray
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class CountSameValuesInArray
+    {
+        public static void Main()
+        {
+            double[] input = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> elements = new Dictionary<double, int>();
+
+            foreach (var number in input)
+            {
+                if (!elements.ContainsKey(number))
+                {
+                    elements.Add(number, 1);
+                }
+                else
+                {
+                    elements[number]++;
+                }
+            }
+
+            foreach (var element in elements)
+            {
+                Console.WriteLine($"{element.Key} - {element.Value} times");
+            }
+        }
+    }
+}
