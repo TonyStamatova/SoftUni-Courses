8c89c3585f79a82bc563e3e4f6393904c450ead3
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-25 14:49:12 +0300

    Added projects

---
+++ C#-Advanced/03.SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedLab/05.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.RecordUniqueNames
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RecordUniqueNames
+    {
+        public static void Main()
+        {
+            var names = new HashSet<string>();
+
+            var numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
