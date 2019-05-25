0e361f2a990223550ce52ce613fc824af41927a4
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-05-25 22:52:41 +0300

    Added folder with problem descriptions & solutions

---
+++ C#-Advanced/03.SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedExercise/01.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.UniqueUsernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            var usernames = new HashSet<string>();
+
+            var numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var username = Console.ReadLine();
+                usernames.Add(username);
+            }
+
+            foreach (var name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
