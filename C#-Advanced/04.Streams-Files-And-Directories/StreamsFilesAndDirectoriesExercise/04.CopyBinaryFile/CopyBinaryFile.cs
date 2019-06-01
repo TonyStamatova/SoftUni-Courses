4a4aad4250ba6bed024f96b73e2151d0e44d0157
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-01 22:08:23 +0300

    Added problem solution

---
+++ C#-Advanced/04.Streams-Files-And-Directories/StreamsFilesAndDirectoriesExercise/04.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.CopyBinaryFile
+{
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        public static void Main()
+        {
+            string directory = "files";
+
+            string picturePath = Path.Combine(directory, "copyMe.png");
+            string copiedPicturePath = Path.Combine(directory, "copyMe-copy.png");
+
+            using (FileStream reader = new FileStream(picturePath, FileMode.Open))
+            {
+                byte[] buffer = new byte[4096];
+
+                int readBytesCount = default;
+
+                while ((readBytesCount = reader.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    using (FileStream writer = new FileStream(copiedPicturePath, FileMode.Append))
+                    {
+                        writer.Write(buffer, 0, readBytesCount);
+                    }
+                }               
+            }
+        }
+    }
+}
