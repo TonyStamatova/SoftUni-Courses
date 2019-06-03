b669fcd7115807314371d29d1a8e7bd3f0a6850a
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-03 12:03:27 +0300

    Added new problem solutions

---
+++ C#-Advanced/04.Streams-Files-And-Directories/StreamsFilesAndDirectoriesExercise/05.DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,50 @@
+﻿namespace _05.DirectoryTraversal
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DirectoryTraversal
+    {
+        public static void Main()
+        {
+            Dictionary<string, Dictionary<string, double>> filesByExtension
+                = new Dictionary<string, Dictionary<string, double>>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(".");
+
+            FileInfo[] allFiles = directoryInfo.GetFiles();
+
+            foreach (var currentFile in allFiles)
+            {                
+                string fileName = currentFile.Name;
+                double fileSize = currentFile.Length / 1024d;
+                string extension = currentFile.Extension;
+
+                if (!filesByExtension.ContainsKey(extension))
+                {
+                    filesByExtension.Add(extension, new Dictionary<string, double>());
+                }
+                
+                filesByExtension[extension].Add(fileName, fileSize);
+            }
+
+            string path =Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop), 
+                "report.txt");
+
+            foreach (var (key, value) in filesByExtension
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key))
+            {
+                File.AppendAllText(path, key + Environment.NewLine);
+
+                foreach (var (name, size) in value.OrderBy(x => x.Value))
+                {
+                    File.AppendAllText(path, $"--{name} - {Math.Round(size, 3)}kb" + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
---
+++ C#-Advanced/04.Streams-Files-And-Directories/StreamsFilesAndDirectoriesExercise/06.ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,24 @@
+﻿namespace _06.ZipAndExtract
+{
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        public static void Main()
+        {
+            string resourceDir = "resources";
+
+            string zipDir = "zippedFiles";
+            Directory.CreateDirectory(zipDir);
+            string zipPath = Path.Combine(zipDir, "copyMe.zip");
+
+            ZipFile.CreateFromDirectory(resourceDir, zipPath);
+
+            string extractDir = "extractedFiles";
+            Directory.CreateDirectory(extractDir);
+
+            ZipFile.ExtractToDirectory(zipPath, extractDir);
+        }
+    }
+}
