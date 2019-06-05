4334f1ef15684278ca7e51cf500cb8b910756b97
TonyStamatova <a.stamatova@abv.bg>
2019-06-05 18:08:17 +0300

    Renamed folder

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingLab/03.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.CountUppercaseWords
+{
+    using System;
+    using System.Linq;
+
+    public class CountUppercaseWords
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> isUppercase = x => char.IsUpper(x[0]);
+
+            string[] filteredWords = input
+                .Where(isUppercase)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, filteredWords));
+        }
+    }
+}
