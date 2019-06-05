87b910cdf6cbc7e1c6d61d903e37ba924b4acf96
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-05 19:07:40 +0300

    Added new problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.ActionPrint
+{
+    using System;
+    using System.Linq;
+
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            Action<string> print = x => Console.WriteLine(x);
+
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(print);
+        }
+    }
+}
