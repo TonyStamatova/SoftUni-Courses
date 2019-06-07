efd3be34ee5c6c1c8ac333dae8a13298aa6491be
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-07 23:14:54 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.CustomMinFunction
+{
+    using System;
+    using System.Linq;
+
+    public class CustomMinFunction
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> findMin = (x, y) => x < y ? x : y;
+
+            int minNumber = int.MaxValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                minNumber = findMin(minNumber, numbers[i]);
+            }
+
+            Action<string> print = x => Console.WriteLine(x);
+
+            print(minNumber.ToString());
+        }
+    }
+}
