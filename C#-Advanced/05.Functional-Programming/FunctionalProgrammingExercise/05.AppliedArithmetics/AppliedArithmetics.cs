6db32be1ca49511d2faac4692ff48a80fa559d3d
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-07 23:15:54 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/05.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.AppliedArithmetics
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class AppliedArithmetics
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<List<int>, Func<int, int>, List<int>> operation = (x,y) => x.Select(y).ToList();
+            Func<int, int> add = x => x + 1;
+            Func<int, int> multiply = x => x * 2;
+            Func<int, int> subtract = x => x - 1;
+
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = operation(numbers, add);
+                        break;
+                    case "multiply":
+                        numbers = operation(numbers, multiply);
+                        break;
+                    case "subtract":
+                        numbers = operation(numbers, subtract);
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                }                
+            }
+        }
+    }
+}
