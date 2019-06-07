c59760ce5ac7efe97220b6fc9299248d622e8076
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-07 23:15:31 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/04.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,43 @@
+﻿namespace _04.FindEvensOrOdds
+{
+    using System;
+    using System.Linq;
+
+    public class FindEvensOrOdds
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int lowerBound = input[0];
+            int upperBound = input[1];
+            string evenOrOdd = Console.ReadLine();
+
+            Predicate<int> condition = x => false;
+            Predicate<int> isEven = x => x % 2 == 0;
+            Predicate<int> isOdd = x => x % 2 != 0;
+
+            switch (evenOrOdd)
+            {
+                case "even":
+                    condition = isEven;
+                    break;
+                case "odd":
+                    condition = isOdd;
+                    break;
+            }
+
+            Action<int> print = x => Console.Write(x + " ");
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (condition(i))
+                {
+                    print(i);
+                }
+            }
+        }
+    }
+}
