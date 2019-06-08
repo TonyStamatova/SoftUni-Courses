533ddd37b1af2fdffd6f23532a75e9f1f3afc3f1
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-08 21:15:15 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/09.ListOfPredicates/ListOfPredicates.cs
@@ -0,0 +1,39 @@
+﻿namespace _09.ListOfPredicates
+{
+    using System;
+    using System.Linq;
+
+    public class ListOfPredicates
+    {
+        public static void Main()
+        {
+            int endOfRange = int.Parse(Console.ReadLine());
+            int[] sequence = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, bool> checkDivision = (x, y) => x % y != 0;
+
+            for (int i = 1; i <= endOfRange; i++)
+            {
+                bool isDivisible = true;
+
+                foreach (var item in sequence)
+                {
+                    if (checkDivision(i, item))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+    }
+}
