6f3ccc2e744c34ef486bd2ab6c283fdd6cfe0f46
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-08 21:14:13 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.PredicateForNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PredicateForNames
+    {
+        public static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, int, bool> filter = (x, y) => x.Length <= y;
+
+            names = names
+                .Where(x => filter(x, length))
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
