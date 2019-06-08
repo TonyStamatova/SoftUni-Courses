ebdecf429196862921240d7ba5cdb53448c87607
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-08 21:14:43 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/08.CustomComparator/CustomComparator.cs
@@ -0,0 +1,31 @@
+﻿namespace _08.CustomComparator
+{
+    using System;
+    using System.Linq;
+
+    public class CustomComparator
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> compare = (x, y)
+                => (x % 2 == 0 && y % 2 != 0)
+                ? -1
+                : (x % 2 != 0 && y % 2 == 0)
+                ? 1
+                : (x < y)
+                ? -1
+                : (x > y)
+                ? 1
+                : 0;
+
+            Array.Sort(numbers, (x, y) => compare(x, y));
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
