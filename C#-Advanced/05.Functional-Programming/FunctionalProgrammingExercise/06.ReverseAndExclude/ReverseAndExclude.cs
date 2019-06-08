cd0e4303895fd6fb23d2dca9013bdfba25c64e42
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-08 21:13:32 +0300

    Added a problem solution

---
+++ C#-Advanced/05.Functional-Programming/FunctionalProgrammingExercise/06.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.ReverseAndExclude
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ReverseAndExclude
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int filterNumber = int.Parse(Console.ReadLine());
+
+            Func<int, int, bool> filter = (x, y) => x % y != 0;
+
+            numbers = numbers.Where(x => filter(x, filterNumber))
+                .Reverse()
+                .ToList();
+
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            print(numbers);
+        }
+    }
+}
