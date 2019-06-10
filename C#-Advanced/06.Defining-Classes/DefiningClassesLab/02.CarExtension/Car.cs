2f11ff9f16d572a4b6251fcced7f783fa0340e5f
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-10 22:45:50 +0300

    Added new folders

---
+++ C#-Advanced/06.Defining-Classes/DefiningClassesLab/02.CarExtension/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double fuelNeededForTrip = distance * this.FuelConsumption / 100;
+            double fuelLeft = this.FuelQuantity - fuelNeededForTrip;
+
+            if (fuelLeft > 0)
+            {
+                this.FuelQuantity -= fuelNeededForTrip;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return
+                $"Make: {this.Make}"
+                + Environment.NewLine + $"Model: {this.Model}"
+                + Environment.NewLine + $"Year: {this.Year}"
+                + Environment.NewLine + $"Fuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
---
+++ C#-Advanced/06.Defining-Classes/DefiningClassesLab/03.CarConstructors/Car.cs
@@ -0,0 +1,71 @@
+﻿namespace CarManufacturer
+{
+    using System;
+
+    public class Car
+    {        
+        private double fuelConsumption;
+        private double fuelQuantity;
+        private string make;
+        private string model;
+        private int year;
+        
+
+        public Car()
+        {
+            this.FuelConsumption = 10;
+            this.FuelQuantity = 200;
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsuption) 
+            : this(make, model, year)
+        {
+            this.FuelConsumption = fuelConsuption;
+            this.FuelQuantity = fuelQuantity;
+        }
+
+        public double FuelConsumption { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }   
+        
+        public void Drive(double distance)
+        {
+            double fuelNeededForTrip = distance * this.FuelConsumption / 100;
+            double fuelLeft = this.FuelQuantity - fuelNeededForTrip;
+
+            if (fuelLeft > 0)
+            {
+                this.FuelQuantity -= fuelNeededForTrip;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return
+                $"Make: {this.Make}"
+                + Environment.NewLine + $"Model: {this.Model}"
+                + Environment.NewLine + $"Year: {this.Year}"
+                + Environment.NewLine + $"Fuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
