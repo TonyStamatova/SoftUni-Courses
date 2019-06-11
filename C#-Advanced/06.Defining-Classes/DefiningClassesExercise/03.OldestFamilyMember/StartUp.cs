d92a0f73d69a28c49b41c01ae18fe2a98165f4c5
TonyStamatova <a.stamatova@abv.bg>
2019-06-11 15:29:32 +0300

    Changed folder location

---
+++ C#-Advanced/06.Defining-Classes/DefiningClassesExercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Family family = new Family();
+
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person newPerson = new Person(name, age);
+                family.AddMember(newPerson);
+            }
+
+            Person oldest = family.GetOldestMember();
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
