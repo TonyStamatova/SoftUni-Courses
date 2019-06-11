32e67fbd5d71e35f6304aa7beabebf2132cb0235
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-11 17:04:51 +0300

    Added new projects

---
+++ C#-Advanced/06.Defining-Classes/DefiningClassesExercise/02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person personOne = new Person();
+            personOne.Name = "Pesho";
+            personOne.Age = 20;
+
+            Person personTwo = new Person("Gosho", 18);
+            Person personThree = new Person("Stamat", 43);
+        }
+    }
+}
