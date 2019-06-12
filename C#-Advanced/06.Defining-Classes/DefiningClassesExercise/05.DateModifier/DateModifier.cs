f3c46cfaf739107701361ae7960f8af28e18c9c0
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-12 22:31:00 +0300

    Added a project

---
+++ C#-Advanced/06.Defining-Classes/DefiningClassesExercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.DateModifier
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    public class DateModifier
+    {
+        private int difference;
+
+        public int Difference
+        {
+            get => this.difference;
+            set => this.difference = value;
+        }
+
+        public void GetDifference(string firstString, string secondString)
+        {
+            DateTime firstDate = DateTime.ParseExact(firstString, "yyyy MM dd", CultureInfo.InvariantCulture);
+            DateTime seconDate = DateTime.ParseExact(secondString, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            TimeSpan span = firstDate.Subtract(seconDate);
+            this.difference = Math.Abs((int)Math.Truncate(span.TotalDays));           
+        }
+    }
+}
---
+++ C#-Advanced/06.Defining-Classes/DefiningClassesExercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _05.DateModifier
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+
+            DateModifier modifier = new DateModifier();
+            modifier.GetDifference(firstString, secondString);
+
+            Console.WriteLine(modifier.Difference);
+        }
+    }
+}
