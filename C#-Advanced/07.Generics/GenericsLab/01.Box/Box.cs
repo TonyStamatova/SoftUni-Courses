347402ac3e1860d76394e9f992e54cda8e7f7d1d
TonyStamatova <a.stamatova@abv.bg>
2019-06-15 12:38:39 +0300

    Added new projects

---
+++ C#-Advanced/07.Generics/GenericsLab/01.Box/Box.cs
@@ -0,0 +1,40 @@
+﻿namespace BoxOfT
+{
+    using System.Collections.Generic;
+
+    public class Box<T>
+    {
+        private Stack<T> content;
+
+        public Box()
+        {
+            this.Content = new Stack<T>();
+        }
+
+        public Stack<T> Content
+        {
+            get => this.content;
+            set => this.content = value;
+        }
+
+        public int Count => this.Content.Count;
+
+        public void Add(T element)
+        {
+            this.Content.Push(element);
+        }
+
+        public T Remove()
+        {
+            if (this.Count > 0)
+            {
+                return this.Content
+                .Pop();
+            }
+            else
+            {
+                return default(T);
+            }
+        }            
+    }
+}
