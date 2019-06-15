d1c09a83fcdef498a34471393ad2f9e8ca1d444a
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-15 22:59:39 +0300

    Added new projects

---
+++ C#-Advanced/07.Generics/GenericsExercise/02.GenericBoxOfIntegers/Box.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.GenericBoxOfIntegers
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            string objType = this.Value.GetType().ToString();
+            return $"{objType}: {this.Value}";
+        }
+    }
+}
