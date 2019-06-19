7dbbad170bb43b715186ff358da52142df72f228
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-19 11:37:15 +0300

    Added new folder

---
+++ C#-Advanced/09.Iterators-And-Comparators/IteratorsAndComparatorsLab/01.Library/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
---
+++ C#-Advanced/09.Iterators-And-Comparators/IteratorsAndComparatorsLab/04.BookComparator/Book.cs
@@ -0,0 +1,32 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            BookComparator comparator = new BookComparator();
+            return comparator.Compare(this, other);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
