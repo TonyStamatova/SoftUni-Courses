a6cb993517cbe54f8d6c6eb0e85a023e21a2793e
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-20 10:23:20 +0300

    Added a project

---
+++ C#-Advanced/09.Iterators-And-Comparators/IteratorsAndComparatorsExercise/07.CustomLinkedList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCustomDoublyLinkedList
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddLast("d");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
