a80fdebf0038aa8f9e1145ae4ebe9cbf5cc4d293
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-20 22:12:22 +0300

    Added a project

---
+++ C#-Advanced/Exam-Solutions/CSharpAdvancedExam24February2019/03.Heroes/HeroRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private Dictionary<string, Hero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new Dictionary<string, Hero>();
+        }
+
+        public int Count => this.heroes.Count;
+
+        public void Add(Hero hero)
+        {
+            this.heroes.Add(hero.Name, hero);
+        }
+
+        public void Remove(string name)
+        {
+            if (this.heroes.ContainsKey(name))
+            {
+                this.heroes.Remove(name);
+            }
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            return this.heroes
+                .Values
+                .OrderByDescending(h => h.Item.Strength)
+                .FirstOrDefault();
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            return this.heroes
+                .Values
+                .OrderByDescending(h => h.Item.Ability)
+                .FirstOrDefault();
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            return this.heroes
+                .Values
+                .OrderByDescending(h => h.Item.Intelligence)
+                .FirstOrDefault();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var (name, hero) in this.heroes)
+            {
+                builder.AppendLine(hero.ToString());
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ C#-Advanced/Exam-Solutions/CSharpAdvancedExam24February2019/03.Heroes/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Item
+    {
+        //        Strength: int
+        //Ability: int
+        //Intelligence: int
+        public int Strength { get; set; }
+
+        public int Ability { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public Item(int strength, int ability, int intelligence)
+        {
+            this.Strength = strength;
+            this.Ability = ability;
+            this.Intelligence = intelligence;
+        }
+
+        public override string ToString()
+        {
+            return $"Item:"
+                + Environment.NewLine + $"  * Strength: {this.Strength}"
+                + Environment.NewLine + $"  * Ability: {this.Ability}"
+                + Environment.NewLine + $"  * Intelligence: {this.Intelligence}";
+        }
+    }
+}
