2635912b95fdb3026e6bb840bda202d2c83f9f6a
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-21 09:04:36 +0300

    Updated file

--- C#-Advanced/Exam-Solutions/CSharpAdvancedExam24February2019/03.Heroes/Item.cs
+++ C#-Advanced/Exam-Solutions/CSharpAdvancedExam24February2019/03.Heroes/Item.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Heroes
 {
+    using System;
+
     public class Item
     {
-        //        Strength: int
-        //Ability: int
-        //Intelligence: int
-        public int Strength { get; set; }
-
-        public int Ability { get; set; }
-
-        public int Intelligence { get; set; }
-
         public Item(int strength, int ability, int intelligence)
         {
             this.Strength = strength;
             this.Ability = ability;
             this.Intelligence = intelligence;
         }
+
+        public int Strength { get; set; }
+
+        public int Ability { get; set; }
+
+        public int Intelligence { get; set; }
 
         public override string ToString()
         {
