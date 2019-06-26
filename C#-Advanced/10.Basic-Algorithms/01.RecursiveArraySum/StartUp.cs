47c27169d3cbf36c18c5a4cd8f148f0965513bbe
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-06-26 23:57:42 +0300

    Added new projects

---
+++ C#-Advanced/10.Basic-Algorithms/01.RecursiveArraySum/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.RecursiveArraySum
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine()
+                  .Split()
+                  .Select(int.Parse)
+                  .ToArray();
+
+            Console.WriteLine(Sum(input, 0));
+        }
+
+        public static int Sum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + Sum(array, ++index);
+        }
+    }
+}
---
+++ C#-Advanced/10.Basic-Algorithms/02.RecursiveFactorial/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.RecursiveFactorial
+{
+    using System;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        private static BigInteger Factorial(BigInteger number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(--number);
+        }
+    }
+}
