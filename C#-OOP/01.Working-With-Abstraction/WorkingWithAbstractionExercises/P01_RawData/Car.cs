346fe3b01c9d5322f75d28185c109502a1ed9ffd
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-02 23:04:59 +0300

    Added new project

---
+++ C#-OOP/01.Working-With-Abstraction/WorkingWithAbstractionExercises/P01_RawData/Car.cs
@@ -0,0 +1,39 @@
+﻿namespace P01_RawData
+{
+    using System.Collections.Generic;
+
+    public class Car
+    {
+        public Car(params string[] parameters)
+        {
+            string model = parameters[0];
+            this.Model = model;
+
+            int engineSpeed = int.Parse(parameters[1]);
+            int enginePower = int.Parse(parameters[2]);
+            this.Engine = new Engine(engineSpeed, enginePower);
+
+            int cargoWeight = int.Parse(parameters[3]);
+            string cargoType = parameters[4];       
+            this.Cargo = new Cargo(cargoWeight, cargoType);
+
+            this.Tires = new List<Tire>(4);
+
+            for (int i = 5; i < parameters.Length; i += 2)
+            {
+                double pressure = double.Parse(parameters[i]);
+                int age = int.Parse(parameters[i + 1]);
+                Tire newTire = new Tire(pressure, age);
+                this.Tires.Add(newTire);
+            }    
+        }
+
+        public string Model { get; private set; }
+
+        public Engine Engine { get; private set; }
+
+        public Cargo Cargo { get; private set; }
+
+        public List<Tire> Tires { get; private set; }
+    }
+}
---
+++ C#-OOP/01.Working-With-Abstraction/WorkingWithAbstractionExercises/P01_RawData/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace P01_RawData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Car> cars = new List<Car>();
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);                
+                cars.Add(new Car(parameters));
+            }
+
+            string command = Console.ReadLine();
+            if (command == "fragile")
+            {
+                List<string> fragile = cars
+                    .Where(x => x.Cargo.Type == "fragile" && x.Tires.Any(y => y.Pressure < 1))
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, fragile));
+            }
+            else
+            {
+                List<string> flamable = cars
+                    .Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250)
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, flamable));
+            }
+        }
+    }
+}
