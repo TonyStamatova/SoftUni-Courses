27716769e177d0931fa2f4b948ddd48527dbec11
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-02 23:06:12 +0300

    Added new project

---
+++ C#-OOP/01.Working-With-Abstraction/WorkingWithAbstractionExercises/P02_CarsSalesman/Car.cs
@@ -0,0 +1,59 @@
+﻿namespace P02_CarsSalesman
+{
+    using System.Text;
+
+    public class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+
+            this.Weight = null;
+            this.Color = "n/a";
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+
+            this.Color = "n/a";
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine)
+        {
+            this.Weight = null;
+
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+            :this(model, engine, weight)
+        {
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+        public Engine Engine { get; private set; }
+        public int? Weight { get; private set; }
+        public string Color { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Model}:");
+
+            sb.Append(this.Engine.ToString());
+
+            string weightAsString = this.Weight.ToString() != string.Empty ? this.Weight.ToString() : "n/a";
+            sb.AppendLine($"  Weight: {weightAsString}");
+
+            sb.Append($"  Color: {this.Color}");
+
+            return sb.ToString();
+        }
+    }
+}
