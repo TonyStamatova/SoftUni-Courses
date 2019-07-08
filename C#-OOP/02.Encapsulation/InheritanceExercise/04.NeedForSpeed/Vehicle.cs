b25feb33ebbf5150abcd958099f26eda1ed5b688
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-08 22:24:21 +0300

    Added new projects

---
+++ C#-OOP/02.Encapsulation/InheritanceExercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DEFAULT_FUEL_CONSUMPTION = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption => DEFAULT_FUEL_CONSUMPTION;
+
+        public double Fuel { get; private set; }
+
+        public int HorsePower { get; private set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            double fuelNeeded = this.FuelConsumption * kilometers;
+
+            if (fuelNeeded <= this.Fuel)
+            {
+                this.Fuel -= fuelNeeded;
+            }
+        }
+    }
+}
