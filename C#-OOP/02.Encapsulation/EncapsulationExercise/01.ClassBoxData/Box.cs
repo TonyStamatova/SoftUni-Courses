b8df3ae05dea8f4cee32d94cd9522c04a15c31d6
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-09 22:12:24 +0300

    Added new folder

---
+++ C#-OOP/02.Encapsulation/EncapsulationExercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,88 @@
+﻿namespace _01.ClassBoxData
+{
+    using System;
+    
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        private double Length
+        {
+            get => this.length;
+
+             set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception($"{nameof(this.Length)} cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        private double Width
+        {
+            get => this.width;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception($"{nameof(this.Width)} cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        private double Height
+        {
+            get => this.height;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception($"{nameof(this.Height)} cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        private double GetSurfaceArea()
+        {
+            return 2 * this.Length * this.Width +
+                   2 * this.Length * this.Height +
+                   2 * this.Width * this.Height;
+        }
+
+        private double GetLateralSurfaceArea()
+        {
+            return 2 * this.Length * this.Height +
+                   2 * this.Width * this.Height;
+        }
+
+        private double GetVolume()
+        {
+            return this.Length * this.Height * this.Width;
+        }
+
+        public string Results()
+        {
+            return $"Surface Area - {GetSurfaceArea():f2}"
+                   + Environment.NewLine + $"Lateral Surface Area - {GetLateralSurfaceArea():f2}"
+                   + Environment.NewLine + $"Volume - {GetVolume():f2}";
+        }
+    }
+}
---
+++ C#-OOP/02.Encapsulation/EncapsulationExercise/01.ClassBoxData/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.ClassBoxData
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box newBox = new Box(length, width, height);
+                Console.WriteLine(newBox.Results());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
