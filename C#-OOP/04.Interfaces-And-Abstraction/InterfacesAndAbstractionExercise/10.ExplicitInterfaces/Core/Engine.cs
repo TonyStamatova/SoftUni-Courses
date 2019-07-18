e6cd56cb514ff25551a833f18a38709872033c00
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-18 15:13:27 +0300

    Added a project

---
+++ C#-OOP/04.Interfaces-And-Abstraction/InterfacesAndAbstractionExercise/10.ExplicitInterfaces/Core/Engine.cs
@@ -0,0 +1,30 @@
+﻿namespace _10.ExplicitInterfaces.Core
+{
+    using System;
+    using _10.ExplicitInterfaces.Contracts;
+    using _10.ExplicitInterfaces.Models;
+
+    public static class Engine
+    {
+        public static void Run()
+        {
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] citizenInfo = input.Split();
+                string name = citizenInfo[0];
+                string country = citizenInfo[1];
+                int age = int.Parse(citizenInfo[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+
+                IPerson person = (IPerson)citizen;
+                Console.WriteLine(person.GetName());
+
+                IResident resident = (IResident)citizen;
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
---
+++ C#-OOP/04.Interfaces-And-Abstraction/InterfacesAndAbstractionExercise/10.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace _10.ExplicitInterfaces
+{
+    using _10.ExplicitInterfaces.Core;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Engine.Run();
+        }
+    }
+}
