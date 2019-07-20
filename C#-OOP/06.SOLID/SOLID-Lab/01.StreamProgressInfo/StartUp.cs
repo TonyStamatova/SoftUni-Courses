e3697204d6136773dd72659980e4882b152d16fb
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-20 19:35:03 +0300

    Added new folder and project

---
+++ C#-OOP/06.SOLID/SOLID-Lab/01.StreamProgressInfo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace P01.Stream_Progress
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            Music music = new Music("ala", "bala", 34, 453);
+            StreamProgressInfo info = new StreamProgressInfo(music);
+            Console.WriteLine(info.CalculateCurrentPercent());
+        }
+    }
+}
---
+++ C#-OOP/06.SOLID/SOLID-Lab/01.StreamProgressInfo/StreamProgressInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace P01.Stream_Progress
+{
+    using P01.Stream_Progress.Contracts;
+
+    public class StreamProgressInfo
+    {
+        private IStreamable streamedItem;
+
+        public StreamProgressInfo(IStreamable streamedItem)
+        {
+            this.streamedItem = streamedItem;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.streamedItem.BytesSent * 100) / this.streamedItem.Length;
+        }
+    }
+}
