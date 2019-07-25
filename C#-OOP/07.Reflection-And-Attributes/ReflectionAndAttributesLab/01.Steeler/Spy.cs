3c411a237a11af55d107afd9ef33206c86ac7d8f
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-25 10:37:16 +0300

    Added new folder

---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesLab/01.Steeler/Spy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string nameOfSuspectClass, params string[] fields)
+    {
+        Type suspectClassType = Type.GetType(nameOfSuspectClass);
+
+        FieldInfo[] allFields = suspectClassType.GetFields(
+            BindingFlags.Static
+            | BindingFlags.Instance
+            | BindingFlags.Public
+            | BindingFlags.NonPublic);
+
+        object suspect = Activator.CreateInstance(suspectClassType, new object[] { });
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.AppendLine(
+            $"Class under investigation: {nameOfSuspectClass}");
+
+        foreach (var field in allFields)
+        {
+            if (fields.Contains(field.Name))
+            {
+                builder.AppendLine($"{field.Name} = {field.GetValue(suspect)}");
+            }
+        }
+
+        return builder
+            .ToString()
+            .TrimEnd();
+    }
+}
+
