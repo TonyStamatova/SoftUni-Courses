66d5067225c79ed3c945620447787be2d05a8499
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-25 12:23:15 +0300

    Added a project

---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesLab/02.HighQualityMistakes/Spy.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string nameOfSuspectClass, params string[] fields)
+    {
+        Type suspectClassType = Type.GetType(nameOfSuspectClass);
+
+        FieldInfo[] allFields = suspectClassType.GetFields(
+            BindingFlags.Static
+            | BindingFlags.Instance
+            | BindingFlags.Public);
+
+        object suspect = Activator.CreateInstance(suspectClassType, new object[] { });
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.AppendLine(
+            $"Class under investigation: {nameOfSuspectClass}");
+
+        foreach (var field in allFields)
+        {
+            if (fields.Contains(field.Name))
+            {
+                builder.AppendLine($"{field.Name} = {field.GetValue(suspect)}");
+            }
+        }
+
+        return builder
+            .ToString()
+            .TrimEnd();
+    }
+
+    public string AnalyzeAcessModifiers(string className)
+    {
+        Type suspectClassType = Type.GetType(className);
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.AppendLine(CheckFieldModifiers(suspectClassType));
+        
+        builder.AppendLine(CheckPropertyMethodModifiers(suspectClassType));
+        
+        return builder.ToString().TrimEnd();
+    }
+
+    private string CheckPropertyMethodModifiers(Type suspectClassType)
+    {
+        PropertyInfo[] propInfo = suspectClassType.GetProperties(
+                    BindingFlags.Static
+                    | BindingFlags.Instance
+                    | BindingFlags.Public
+                    | BindingFlags.NonPublic);
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.AppendLine(CheckGetterModifiers(propInfo));
+        builder.AppendLine(CheckSetterModifiers(propInfo));
+
+        return builder.ToString().TrimEnd();
+    }
+
+    private string CheckSetterModifiers(PropertyInfo[] propInfo)
+    {
+        MethodInfo[] settersInfo = propInfo
+                    .Select(x => x.GetSetMethod())
+                    .Where(x => x != null)
+                    .ToArray();
+
+        StringBuilder builder = new StringBuilder();
+
+        foreach (MethodInfo setter in settersInfo)
+        {
+            if (setter.IsPublic)
+            {
+                builder.AppendLine($"{setter.Name} have to be private!");
+            }
+        }
+
+        return builder.ToString().TrimEnd();
+    }
+
+    private static string CheckGetterModifiers(PropertyInfo[] propInfo)
+    {
+        MethodInfo[] gettersInfo = propInfo
+                    .Select(x => x.GetGetMethod(true))
+                    .ToArray();
+
+        StringBuilder builder = new StringBuilder();
+
+        foreach (MethodInfo getter in gettersInfo)
+        {
+            if (!getter.IsPublic)
+            {
+                builder.AppendLine($"{getter.Name} have to be public!");
+            }
+        }
+
+        return builder.ToString().TrimEnd();
+    }
+
+    private string CheckFieldModifiers(Type suspectClassType)
+    {
+        FieldInfo[] allFields = suspectClassType.GetFields(
+                    BindingFlags.Static
+                    | BindingFlags.Instance
+                    | BindingFlags.Public);
+
+        StringBuilder builder = new StringBuilder();
+
+        foreach (FieldInfo field in allFields)
+        {
+            if (!field.IsPrivate)
+            {
+                builder.AppendLine($"{field.Name} must be private!");
+            }
+        }
+
+        return builder.ToString().TrimEnd();
+    }
+}
+
