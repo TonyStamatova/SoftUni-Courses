cbdaaa2cde35c9084956352765b4e3dd4b130659
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-26 15:22:29 +0300

    Added  project

---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesLab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Tracker
+{
+    public void PrintMethodsByAuthor()
+    {
+        Type type = typeof(StartUp);
+
+        MethodInfo[] methods = type.GetMethods(
+            BindingFlags.Instance
+            | BindingFlags.Static
+            | BindingFlags.Public
+            | BindingFlags.NonPublic);
+
+        foreach (MethodInfo method in methods)
+        {
+            Attribute[] attributes = method
+                .GetCustomAttributes(typeof(AuthorAttribute))
+                .ToArray();
+
+            foreach (AuthorAttribute attribute in attributes)
+            {
+                Console.WriteLine($"{method.Name} is wirtten by {attribute.Name}");
+            }
+        }
+    }
+}
