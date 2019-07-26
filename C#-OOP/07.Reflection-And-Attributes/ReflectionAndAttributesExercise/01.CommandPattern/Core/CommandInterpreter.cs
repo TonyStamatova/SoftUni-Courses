01b762473f86a53d29c1b2f0179cbc4185503cd0
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-26 18:05:22 +0300

    Added new folder

---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesExercise/01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,37 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using CommandPattern.Core.Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandInfo = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string commandTypeName = commandInfo[0] + "Command";
+            string[] commandArgs = commandInfo
+                .Skip(1)
+                .ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            Type[] allTypes = assembly.GetTypes();
+
+            Type commandType = allTypes.FirstOrDefault(t => t.Name == commandTypeName);
+
+            if (commandType == null)
+            {
+                throw new ArgumentException();
+            }
+
+            ICommand command = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = command.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesExercise/01.CommandPattern/Core/Engine.cs
@@ -0,0 +1,35 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+
+    using CommandPattern.Core.Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            string input = string.Empty;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                try
+                {
+                    string result = this.commandInterpreter.Read(input);
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException)
+                {
+                }
+            }
+        }
+    }
+}
