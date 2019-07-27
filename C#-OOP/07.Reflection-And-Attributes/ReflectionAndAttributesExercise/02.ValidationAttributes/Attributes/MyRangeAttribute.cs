b44c8ec389b6946e7523b458b4fb107517bac339
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-27 15:33:30 +0300

    Added new project

---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesExercise/02.ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+        private int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if (!int.TryParse(obj.ToString(), out int parameterAsInt))
+            {
+                return false;
+            }
+
+            if (parameterAsInt >= minValue && parameterAsInt <= maxValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C#-OOP/07.Reflection-And-Attributes/ReflectionAndAttributesExercise/02.ValidationAttributes/Core/Validator.cs
@@ -0,0 +1,39 @@
+﻿namespace ValidationAttributes.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using ValidationAttributes.Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objectType = obj.GetType();
+            
+            PropertyInfo[] properties = objectType.GetProperties(
+                BindingFlags.Instance
+                | BindingFlags.Static
+                | BindingFlags.Public
+                | BindingFlags.NonPublic);
+
+            foreach (PropertyInfo property in properties)
+            {
+                Attribute[] attributes = property
+                     .GetCustomAttributes()
+                     .ToArray();
+
+                foreach (MyValidationAttribute attr in attributes)
+                {
+                    if (!attr.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
