a94cbe05bdf002b45035528165b88210c6581112
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-28 19:17:44 +0300

    Added new problem solution

---
+++ Algorithms/01.Recursion/RecursionLab/P05GeneratingCombinations/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace P05GeneratingCombinations
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] set = Console.ReadLine().Split();
+
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            LoopNumberOfLoops(set, 0, new string[elementsCount], 0);
+        }
+
+        public static void LoopNumberOfLoops(
+            string[] wholeSet,
+            int setIndex,
+            string[] result,
+            int resultIndex)
+        {
+            if (resultIndex == result.Length)
+            {
+                Console.WriteLine(string.Join(" ", result));
+                return;
+            }
+
+            for (int i = setIndex; i < wholeSet.Length; i++)
+            {
+                result[resultIndex] = wholeSet[i];
+                LoopNumberOfLoops(wholeSet, i + 1, result, resultIndex + 1);               
+            }
+        }
+    }
+}
