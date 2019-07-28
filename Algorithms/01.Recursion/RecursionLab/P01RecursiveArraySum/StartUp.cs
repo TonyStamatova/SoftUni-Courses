14a826d828ce24ce0a3c858724113a2e5923974f
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-28 11:14:14 +0300

    Added folder for new course

---
+++ Algorithms/01.Recursion/RecursionLab/P01RecursiveArraySum/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace P01RecursiveArraySum
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Sum(array, 0));
+        }
+
+        public static int Sum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + Sum(array, index + 1);
+        }
+    }
+}
