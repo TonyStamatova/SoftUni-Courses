0525f448b711d342a72e84bdb39e19bb08dc1238
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-28 11:23:00 +0300

    Added new problem solution

---
+++ Algorithms/01.Recursion/RecursionLab/P02RecursiveFactorial/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace P02RecursiveFactorial
+{
+    using System;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorial(number));
+        }
+
+        public static BigInteger GetFactorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * GetFactorial(number - 1);
+        }
+    }
+}
