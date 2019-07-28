6d5b7419a1f1299cc25db2fc391eb025237c29a5
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-28 13:21:23 +0300

    Added new problem solution

---
+++ Algorithms/01.Recursion/RecursionLab/P04Generating01Vectors/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace P04Generating01Vectors
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[length];
+
+            GenerateCombinations(vector, 0);
+        }
+
+        public static void GenerateCombinations(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {                
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+
+            for (int i = 0; i <=1; i++)
+            {
+                vector[index] = i;
+                GenerateCombinations(vector, index + 1);
+            }
+        }
+    }
+}
