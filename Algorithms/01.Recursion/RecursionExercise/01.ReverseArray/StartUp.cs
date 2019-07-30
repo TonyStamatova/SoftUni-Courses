a243aa3b104155edc3bd6ed2c5aa584e07cba68c
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-30 14:34:04 +0300

    Added new folder

---
+++ Algorithms/01.Recursion/RecursionExercise/01.ReverseArray/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.ReverseArray
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] array = Console.ReadLine().Split();
+
+            Print(array, 0);
+        }
+
+        private static void Print(string[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return;
+            }
+
+            Print(array, index + 1);
+            Console.Write(array[index] + " ");
+        }
+    }
+}
---
+++ Algorithms/01.Recursion/RecursionExercise/02.NestedLoopsToRecursion/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.NestedLoopsToRecursion
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfLoops = int.Parse(Console.ReadLine());
+
+            Loop(new int[numberOfLoops],  0);
+        }
+
+        private static void Loop(int[] loopSequence, int index)
+        {
+            if (index == loopSequence.Length)
+            {
+                Console.WriteLine(string.Join(" ",loopSequence));
+                return;
+            }
+
+            for (int i = 1; i <= loopSequence.Length; i++)
+            {
+                loopSequence[index] = i;
+                Loop(loopSequence, index + 1);
+            }
+        }
+    }
+}
