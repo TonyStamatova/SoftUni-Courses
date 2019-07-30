3b96e140501a212954d4b6add1d6cf93b9bb1c1c
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-30 17:04:27 +0300

    Added new problem solution

---
+++ Algorithms/01.Recursion/RecursionExercise/03.CombinationsWithRepetition/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.CombinationsWithRepetition
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int setSize = int.Parse(Console.ReadLine());
+            int combinationSize = int.Parse(Console.ReadLine());
+
+            GenerateCombination(1, setSize, new int[combinationSize], 0);
+        }
+
+        private static void GenerateCombination(
+            int startIndex, 
+            int setSize, 
+            int[] combination, 
+            int combinationIndex)
+        {
+            if (combinationIndex == combination.Length)
+            {
+                Console.WriteLine(string.Join(" ", combination));
+                return;
+            }
+
+            for (int i = startIndex; i <= setSize; i++)
+            {
+                combination[combinationIndex] = i;
+                GenerateCombination(startIndex, setSize, combination, combinationIndex + 1);
+                startIndex++;
+            }
+        }
+    }
+}
