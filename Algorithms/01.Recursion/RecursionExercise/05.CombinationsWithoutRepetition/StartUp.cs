4cbdac715b637ef90bad90c8eddd92a748cf5bf2
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-07-31 14:37:05 +0300

    Added new project

---
+++ Algorithms/01.Recursion/RecursionExercise/05.CombinationsWithoutRepetition/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.CombinationsWithoutRepetition
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int setSize = int.Parse(Console.ReadLine());
+            int combinationSize = int.Parse(Console.ReadLine());
+
+            GenerateCombination(1, setSize, new int[combinationSize], 0);
+        }
+
+        private static void GenerateCombination(
+            int startIndex, 
+            int setSize, 
+            int[] combination, 
+            int combinationIndex)
+        {
+            if (combinationIndex == combination.Length)
+            {
+                Console.WriteLine(string.Join(" ", combination));
+                return;
+            }
+
+            for (int i = startIndex; i <= setSize; i++)
+            {
+                combination[combinationIndex] = i;
+                GenerateCombination(startIndex + 1, setSize, combination, combinationIndex + 1);
+                startIndex++;
+            }
+        }
+    }
+}
