acbfbaf605513a53ad837e3ea1ccde723f261ded
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-08-02 18:19:22 +0300

    Added new project

---
+++ C#-OOP/11.Unit-Testing/UnitTestingLab/Tests/AxeTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private static Dummy dummy;
+
+        [SetUp]
+        public static void CreateAxeAndDummy()
+        {
+            dummy = new Dummy(10, 10);
+        }
+
+        [Test]
+        public static void WeaponLoosesDurabilityAfterAttack()
+        {
+            Axe axe = new Axe(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9));
+        }
+
+        [Test]
+        public static void AttackWithBrokenAxeShouldThrow()
+        {
+            Axe axe = new Axe(10, 0);
+
+            Assert.That(
+                () => axe.Attack(dummy), 
+                Throws.InvalidOperationException.With.Message
+                    .EqualTo("Axe is broken."));
+        }
+    }
+}
