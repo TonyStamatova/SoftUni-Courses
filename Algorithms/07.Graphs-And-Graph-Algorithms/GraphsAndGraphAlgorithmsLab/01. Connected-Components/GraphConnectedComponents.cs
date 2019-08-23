fdc44b5d9f6262986c4408c253a22f92ca37d874
TonyStamatova <a.stamatova@abv.bg>
2019-08-23 23:16:09 +0300

    Added new folder

---
+++ Algorithms/07.Graphs-And-Graph-Algorithms/GraphsAndGraphAlgorithmsLab/01. Connected-Components/GraphConnectedComponents.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph;
+    static bool[] visited;
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        visited = new bool[graph.Length];
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (visited[node])
+            {
+                continue;
+            }
+
+            Console.Write("Connected component:");
+
+            PerformDFSForCurrentNode(node);
+
+            Console.WriteLine();
+        }
+    }
+
+    private static void PerformDFSForCurrentNode(int node)
+    {
+        visited[node] = true;
+
+        List<int> connections = graph[node];
+
+        if (connections.All(c => visited[c]))
+        {
+            Console.Write($" {node}");
+            return;
+        }
+
+        foreach (var connection in connections.Where(c => !visited[c]))
+        {
+            PerformDFSForCurrentNode(connection);
+        }
+
+        Console.Write($" {node}");
+    }
+}
