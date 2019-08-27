fb296e66ca104e243302742f79c3c53844ccec31
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-08-27 12:07:54 +0300

    Added new problem solution

---
+++ C#-OOP/08.Exceptions-And-Error-Handling/ExceptionsAndErrorHandlingLab/05.ConvertToDouble/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace _05.ConvertToDouble
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                TriggerFormatException();
+            }
+            catch (FormatException)
+            {                
+            }
+
+            try
+            {
+                TriggerOverflowException();
+            }
+            catch (OverflowException)
+            {
+            }
+        }
+
+        private static void TriggerFormatException()
+        {
+            string invalidNumberFormat = "this string is not a number in a valid format";
+
+            try
+            {
+                double result = Convert.ToDouble(invalidNumberFormat);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+                throw fe;
+            }            
+        }
+
+        private static void TriggerOverflowException()
+        {
+            string biggerThanMaxValue = "2.79769313486232E+308";
+
+            try
+            {
+                double result = Convert.ToDouble(biggerThanMaxValue);
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+                throw oe;
+            }
+        }
+    }
+}
