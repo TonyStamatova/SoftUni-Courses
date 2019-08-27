22cfd49272bc76b5b9dba1afaff343babad528a6
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-08-27 11:29:27 +0300

    Added  new problem solution

---
+++ C#-OOP/08.Exceptions-And-Error-Handling/ExceptionsAndErrorHandlingLab/04.FixingVolTwo/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.FixingVolTwo
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+
+            try
+            {                
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+                int resultAsInt = num1 * num2;
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, resultAsInt);
+            }  
+
+            Console.ReadLine();
+        }
+    }
+}
