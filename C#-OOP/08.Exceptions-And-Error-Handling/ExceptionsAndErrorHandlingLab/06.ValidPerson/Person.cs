f09831e4dd8dffc4ca4134fc0715ee377040a078
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-08-27 12:39:34 +0300

    Added new problem solution

---
+++ C#-OOP/08.Exceptions-And-Error-Handling/ExceptionsAndErrorHandlingLab/06.ValidPerson/Person.cs
@@ -0,0 +1,62 @@
+﻿namespace _06.ValidPerson
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get => this.firstName;
+            set
+            {
+                ValidateName(value);
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            set
+            {
+                ValidateName(value);
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be an integer between 0 and 120!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        private static void ValidateName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("value", "Name should not be null or empty!");
+            }
+        }
+    }
+}
---
+++ C#-OOP/08.Exceptions-And-Error-Handling/ExceptionsAndErrorHandlingLab/06.ValidPerson/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace _06.ValidPerson
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string[]> peopleInfo = new List<string[]>
+            {
+                new string[]{ "Pesho", "Peshev", "24" },
+                new string[]{ string.Empty, "Goshev", "31" },
+                new string[]{ "Ivan", null, "63" },
+                new string[]{ "Stoyan", "Kolev", "-1" },
+                new string[]{ "Iskren", "Ivanov", "121" }
+            };
+
+
+            foreach (var personInfo in peopleInfo)
+            {
+                try
+                {
+                    CreateNewPerson(personInfo);
+                }
+                catch (ArgumentNullException ane)
+                {
+                    PrintExceptionMessage(ane);
+                    continue;
+                }
+                catch (ArgumentOutOfRangeException aoore)
+                {
+                    PrintExceptionMessage(aoore);
+                    continue;
+                }
+
+                Console.WriteLine("You have created a Person entity successfully");
+            }           
+        }
+
+        private static void CreateNewPerson(string[] personInfo)
+        {
+            string firstName = personInfo[0];
+            string lastName = personInfo[1];
+            int age = int.Parse(personInfo[2]);
+
+            Person newPerson = new Person(firstName, lastName, age);
+        }
+
+        private static void PrintExceptionMessage(ArgumentException ae)
+        {
+            Console.WriteLine($"Exception thrown: {ae.Message}");
+        }
+    }
+}
