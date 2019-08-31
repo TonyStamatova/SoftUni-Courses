7c547ba46b41029da80db97ba46aaa78288bec1d
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-08-31 09:39:50 +0300

    Updated files

--- Algorithms/08.Advanced-Graph-Algorithms-Part-One/AdvancedGraphAlgorithmsPartOneLab/Dijkstra/DijkstraMain.cs
+++ Algorithms/08.Advanced-Graph-Algorithms-Part-One/AdvancedGraphAlgorithmsPartOneLab/Dijkstra/DijkstraMain.cs
@@ -1,13 +1,11 @@
-﻿namespace Dijkstra
-{
-    using System;
+﻿using System;
 
-    public class DijkstraMain
+public class DijkstraMain
+{
+    public static void Main()
     {
-        public static void Main()
+        var graph = new[,]
         {
-            var graph = new[,]
-            {
                 // 0   1   2   3   4   5   6   7   8   9  10  11
                 { 0,  0,  0,  0,  0,  0, 10,  0, 12,  0,  0,  0 }, // 0
                 { 0,  0,  0,  0, 20,  0,  0, 26,  0,  5,  0,  6 }, // 1
@@ -23,37 +21,37 @@
                 { 0,  6,  9,  0, 11, 33,  0, 20,  0,  0,  0,  0 }, // 11
             };
 
-            PrintPath(graph, 0, 9);
-            PrintPath(graph, 0, 2);
-            PrintPath(graph, 0, 10);
-            PrintPath(graph, 0, 11);
-            PrintPath(graph, 0, 1);
-        }
+        PrintPath(graph, 0, 9);
+        PrintPath(graph, 0, 2);
+        PrintPath(graph, 0, 10);
+        PrintPath(graph, 0, 11);
+        PrintPath(graph, 0, 1);
+    }
 
-        public static void PrintPath(int[,] graph, int sourceNode, int destinationNode)
+    public static void PrintPath(int[,] graph, int sourceNode, int destinationNode)
+    {
+        Console.Write(
+            "Shortest path [{0} -> {1}]: ",
+            sourceNode,
+            destinationNode);
+
+        var path = DijkstraWithoutQueue.DijkstraAlgorithm(graph, sourceNode, destinationNode);
+
+        if (path == null)
         {
-            Console.Write(
-                "Shortest path [{0} -> {1}]: ",
-                sourceNode,
-                destinationNode);
-
-            var path = DijkstraWithoutQueue.DijkstraAlgorithm(graph, sourceNode, destinationNode);
-
-            if (path == null)
+            Console.WriteLine("no path");
+        }
+        else
+        {
+            int pathLength = 0;
+            for (int i = 0; i < path.Count - 1; i++)
             {
-                Console.WriteLine("no path");
+                pathLength += graph[path[i], path[i + 1]];
             }
-            else
-            {
-                int pathLength = 0;
-                for (int i = 0; i < path.Count - 1; i++)
-                {
-                    pathLength += graph[path[i], path[i + 1]];
-                }
 
-                var formattedPath = string.Join("->", path);
-                Console.WriteLine("{0} (length {1})", formattedPath, pathLength);
-            }
+            var formattedPath = string.Join("->", path);
+            Console.WriteLine("{0} (length {1})", formattedPath, pathLength);
         }
     }
 }
+
--- Algorithms/08.Advanced-Graph-Algorithms-Part-One/AdvancedGraphAlgorithmsPartOneLab/Dijkstra/DijkstraWithoutQueue.cs
+++ Algorithms/08.Advanced-Graph-Algorithms-Part-One/AdvancedGraphAlgorithmsPartOneLab/Dijkstra/DijkstraWithoutQueue.cs
@@ -1,84 +1,96 @@
-﻿namespace Dijkstra
+﻿using System.Collections.Generic;
+
+public static class DijkstraWithoutQueue
 {
-    using System;
-    using System.Collections.Generic;
+    private static int nodesCount;
 
-    public static class DijkstraWithoutQueue
+    private static int[] shortest;
+    private static int[] previous;
+    private static bool[] visited;
+
+    public static List<int> DijkstraAlgorithm(int[,] graph, int sourceNode, int destinationNode)
     {
-        private static int nodesCount;
-       
-        private static int[] shortest;
-        private static int[] previous;
-        private static bool[] visited;
+        nodesCount = graph.GetLength(0);
+        shortest = new int[nodesCount];
+        previous = new int[nodesCount];
+        visited = new bool[nodesCount];
 
-        public static List<int> DijkstraAlgorithm(int[,] graph, int sourceNode, int destinationNode)
+        PopulateShortestLengthsArray();
+        PopulatePreviousNodesArray();
+        shortest[sourceNode] = 0;
+
+        TreverseNodes(sourceNode, destinationNode, graph);
+
+        return ReconstructSolution(sourceNode, destinationNode);
+    }
+
+    private static void PopulatePreviousNodesArray()
+    {
+        for (int i = 0; i < previous.Length; i++)
         {
-            nodesCount = graph.GetLength(0);
-            shortest = new int[nodesCount];
-            previous = new int[nodesCount];
-            visited = new bool[nodesCount];
-
-            PopulateShortestLengthsArray();
-            shortest[sourceNode] = 0;
-
-            TreverseNodes(sourceNode, destinationNode, graph);
-
-            return ReconstructSolution(destinationNode);
+            previous[i] = -1;
         }
+    }
 
-        private static List<int> ReconstructSolution(int destinationNode)
+    private static List<int> ReconstructSolution(int sourceNode, int destinationNode)
+    {
+        List<int> path = new List<int>();
+        path.Add(destinationNode);
+
+        int currentNode = destinationNode;
+
+        while (currentNode != sourceNode)
         {
-            List<int> path = new List<int>();
-            path.Add(destinationNode);
-
-            int currentNode = destinationNode;
-
-            while (currentNode != 0)
-            {                
-                currentNode = previous[currentNode];
-                path.Add(currentNode);
-            }
-
-            path.Reverse();
-            return path;
-        }
-
-        private static void PopulateShortestLengthsArray()
-        {
-            for (int i = 0; i < shortest.Length; i++)
+            if (currentNode == -1)
             {
-                shortest[i] = int.MaxValue;
+                return null;
             }
+
+            currentNode = previous[currentNode];
+            path.Add(currentNode);
         }
 
-        private static void TreverseNodes(int sourceNode, int destinationNode, int[,] graph)
+        path.Reverse();
+        return path;
+    }
+
+    private static void PopulateShortestLengthsArray()
+    {
+        for (int i = 0; i < shortest.Length; i++)
         {
-            if (sourceNode == destinationNode)
-            {
-                return;
-            }
-
-            visited[sourceNode] = true;
-
-            for (int child = 0; child < nodesCount; child++)
-            {
-                int currentLength = graph[sourceNode, child];
-
-                if (currentLength == 0 || visited[child])
-                {
-                    continue;
-                }
-
-                int sumLength = currentLength + shortest[sourceNode];
-
-                if (sumLength < shortest[child])
-                {
-                    shortest[child] = sumLength;
-                    previous[child] = sourceNode;
-                }
-
-                TreverseNodes(child, destinationNode, graph);
-            }
+            shortest[i] = int.MaxValue;
         }
+    }
+
+    private static void TreverseNodes(int sourceNode, int destinationNode, int[,] graph)
+    {
+        if (sourceNode == destinationNode)
+        {
+            return;
+        }
+
+        visited[sourceNode] = true;
+
+        for (int child = 0; child < nodesCount; child++)
+        {
+            int currentLength = graph[sourceNode, child];
+
+            if (currentLength == 0 || visited[child])
+            {
+                continue;
+            }
+
+            int sumLength = currentLength + shortest[sourceNode];
+
+            if (sumLength < shortest[child])
+            {
+                shortest[child] = sumLength;
+                previous[child] = sourceNode;
+            }
+
+            TreverseNodes(child, destinationNode, graph);
+        }
+
+        visited[sourceNode] = false;
     }
 }
