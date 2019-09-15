f99324c6fa2e856bc95a76065889291e340588d7
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-09-15 13:35:08 +0300

    Added new problem solution

---
+++ Algorithms/11.Problem-Solving-Part-One/ProblemSolvingPartOneLab/02.SumTo13/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.SumTo13
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        private static int[] numbers;
+
+        public static void Main()
+        {
+            numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SumNumber(0, 0);
+
+            Console.WriteLine("No");
+        }
+
+        private static void SumNumber(int index, int sum)
+        {       
+            if (index >= numbers.Length)
+            {
+                if (sum == 13)
+                {
+                    Console.WriteLine("Yes");
+                    Environment.Exit(0);
+                }
+
+                return;
+            }
+
+            SumNumber(index + 1, sum + numbers[index]);
+            SumNumber(index + 1, sum - numbers[index]);
+        }
+    }
+}
