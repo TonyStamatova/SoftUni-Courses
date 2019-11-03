75a6a76e89d361b5f3e04bdeebd2c29376ea8379
TonyStamatova <49076000+TonyStamatova@users.noreply.github.com>
2019-11-03 19:05:10 +0200

    Added new project

---
+++ Entity Framework Core/03.Entity-Framework-Introduction/SoftUni/Data/Results/EmployeeResultModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SoftUni.Data.Results
+{
+    public class EmployeeResultModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"{this.FirstName} {this.LastName} ";
+
+            if (this.MiddleName != null)
+            {
+                result += $"{this.MiddleName} ";
+            } 
+            
+            result += $"{this.JobTitle} {this.Salary:F2}";
+
+            return result;
+        }
+    }
+}
---
+++ Entity Framework Core/03.Entity-Framework-Introduction/SoftUni/Data/Results/ResearchAndDevelopmentResultModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftUni.Data.Results
+{
+    public class ResearchAndDevelopmentResultModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} from {this.DepartmentName} - ${this.Salary:f2}";
+        }
+    }
+}
